df750daed3c266c05aa824afb8256d04baf86b82
azzni96 <nihadazzam96@gmail.com>
2023-11-12 23:16:54 +0200

    testi

--- js_moduli2/TA1.js
+++ js_moduli2/TA1.js
@@ -5,7 +5,6 @@ num3 = parseInt(prompt('Enter numero 3'))
 num4 = parseInt(prompt('Enter numero 4'))
 num5 = parseInt(prompt('Enter numero 5'))
 const numbers =[num1, num2, num3, num4, num5]
-console.log(numbers)
-for (let i=0; i<numbers.length; i++) {
-  console.log(`Num:)
+for (let i = numbers.length - 1; i >= 0; i--) {
+  console.log(`Num:${numbers[i]}`)
 }
